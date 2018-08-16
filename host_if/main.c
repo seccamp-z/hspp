f7f845a8171f3791d0740625f67d093809b463d2
Hiroki Shirokura <slankdev@nttv6.jp>
2018-08-16 17:31:44 +0900

    2018.08.16-17:31

--- host_if/main.c
+++ host_if/main.c
@@ -12,190 +12,88 @@
 
 #define RX_RING_SIZE 1024
 #define TX_RING_SIZE 1024
-
 #define NUM_MBUFS 8191
 #define MBUF_CACHE_SIZE 250
 #define BURST_SIZE 32
 
-static const struct rte_eth_conf port_conf_default = {
-	.rxmode = {
-		.max_rx_pkt_len = ETHER_MAX_LEN,
-		.ignore_offload_bitfield = 1,
-	},
-};
-
-/* basicfwd.c: Basic DPDK skeleton forwarding example. */
-
-/*
- * Initializes a given port using global settings and with the RX buffers
- * coming from the mbuf_pool passed as a parameter.
- */
-static inline int
-port_init(uint16_t port, struct rte_mempool *mbuf_pool)
+static void port_configure(uint8_t port, size_t nb_rxq, size_t nb_txq,
+      const struct rte_eth_conf* port_conf, struct rte_mempool* mp)
 {
-	struct rte_eth_conf port_conf = port_conf_default;
-	const uint16_t rx_rings = 1, tx_rings = 1;
-	uint16_t nb_rxd = RX_RING_SIZE;
-	uint16_t nb_txd = TX_RING_SIZE;
-	int retval;
-	uint16_t q;
-	struct rte_eth_dev_info dev_info;
-	struct rte_eth_txconf txconf;
+  const size_t rx_ring_size = 128;
+  const size_t tx_ring_size = 512;
 
-	if (port >= rte_eth_dev_count())
-		return -1;
+  int ret = rte_eth_dev_configure(port, nb_rxq, nb_txq, port_conf);
+  if (ret < 0) rte_exit(EXIT_FAILURE, "rte_eth_dev_configure\n");
 
-	rte_eth_dev_info_get(port, &dev_info);
-	if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
-		port_conf.txmode.offloads |=
-			DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+  for (size_t q=0; q<nb_rxq; q++) {
+    int ret = rte_eth_rx_queue_setup(port, q, rx_ring_size,
+        rte_eth_dev_socket_id(port), NULL, mp);
+    if (ret < 0) rte_exit(EXIT_FAILURE, "rte_eth_rx_queue_setup\n");
+  }
 
-	/* Configure the Ethernet device. */
-	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-	if (retval != 0)
-		return retval;
+  for (size_t q=0; q<nb_txq; q++) {
+    struct rte_eth_txconf txconf;
+    memset(&txconf, 0x0, sizeof(txconf));
+    txconf.txq_flags &= ~ETH_TXQ_FLAGS_NOMULTSEGS;
+    int ret = rte_eth_tx_queue_setup(port, q, tx_ring_size,
+        rte_eth_dev_socket_id(port), &txconf);
+    if (ret < 0) rte_exit(EXIT_FAILURE, "rte_eth_tx_queue_setup\n");
+  }
 
-	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	if (retval != 0)
-		return retval;
-
-	/* Allocate and set up 1 RX queue per Ethernet port. */
-	for (q = 0; q < rx_rings; q++) {
-		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
-				rte_eth_dev_socket_id(port), NULL, mbuf_pool);
-		if (retval < 0)
-			return retval;
-	}
-
-	txconf = dev_info.default_txconf;
-	txconf.txq_flags = ETH_TXQ_FLAGS_IGNORE;
-	txconf.offloads = port_conf.txmode.offloads;
-	/* Allocate and set up 1 TX queue per Ethernet port. */
-	for (q = 0; q < tx_rings; q++) {
-		retval = rte_eth_tx_queue_setup(port, q, nb_txd,
-				rte_eth_dev_socket_id(port), &txconf);
-		if (retval < 0)
-			return retval;
-	}
-
-	/* Start the Ethernet port. */
-	retval = rte_eth_dev_start(port);
-	if (retval < 0)
-		return retval;
-
-	/* Display the port MAC address. */
-	struct ether_addr addr;
-	rte_eth_macaddr_get(port, &addr);
-	printf("Port %u MAC: %02" PRIx8 " %02" PRIx8 " %02" PRIx8
-			   " %02" PRIx8 " %02" PRIx8 " %02" PRIx8 "\n",
-			port,
-			addr.addr_bytes[0], addr.addr_bytes[1],
-			addr.addr_bytes[2], addr.addr_bytes[3],
-			addr.addr_bytes[4], addr.addr_bytes[5]);
-
-	/* Enable RX in promiscuous mode for the Ethernet device. */
-	rte_eth_promiscuous_enable(port);
-
-	return 0;
+  ret = rte_eth_dev_start(port);
+  if (ret < 0) rte_exit(EXIT_FAILURE, "rte_eth_dev_start\n");
+  rte_eth_promiscuous_enable(port);
 }
 
-/*
- * The lcore main. This is the main thread that does the work, reading from
- * an input port and writing to an output port.
- */
-static __attribute__((noreturn)) void
-lcore_main(void)
+int main(int argc, char *argv[])
 {
-	const uint16_t nb_ports = rte_eth_dev_count();
-	uint16_t port;
+  int ret = rte_eal_init(argc, argv);
+  if (ret < 0) rte_exit(EXIT_FAILURE, "rte_eal_init\n");
 
-	/*
-	 * Check that the port is on the same NUMA node as the polling thread
-	 * for best performance.
-	 */
-	for (port = 0; port < nb_ports; port++)
-		if (rte_eth_dev_socket_id(port) > 0 &&
-				rte_eth_dev_socket_id(port) !=
-						(int)rte_socket_id())
-			printf("WARNING, port %u is on remote NUMA node to "
-					"polling thread.\n\tPerformance will "
-					"not be optimal.\n", port);
+  const uint16_t nb_ports = rte_eth_dev_count();
+  if (nb_ports != 2) rte_exit(EXIT_FAILURE, "Error: #ports isn't 2\n");
 
-	printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
-			rte_lcore_id());
+  struct rte_mempool *mp;
+  mp = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+    MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+  if (mp == NULL) rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Run until the application is quit or killed. */
-	for (;;) {
-		/*
-		 * Receive packets on a port and forward them on the paired
-		 * port. The mapping is 0 -> 1, 1 -> 0, 2 -> 3, 3 -> 2, etc.
-		 */
-		for (port = 0; port < nb_ports; port++) {
+  struct rte_eth_conf port_conf;
+  memset(&port_conf, 0, sizeof(struct rte_eth_conf));
+  port_conf.rxmode.mq_mode = ETH_MQ_RX_NONE;
+  port_conf.rxmode.max_rx_pkt_len = ETHER_MAX_LEN;
+  port_conf.rxmode.header_split = 0;
+  port_conf.rxmode.jumbo_frame = 1;
+  port_conf.rxmode.enable_scatter = 1;
+  port_conf.rxmode.ignore_offload_bitfield = 1;
+  port_configure(0,1,1,&port_conf,mp);
 
-			/* Get burst of RX packets, from first port of pair. */
-			struct rte_mbuf *bufs[BURST_SIZE];
-			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
-					bufs, BURST_SIZE);
+  for (uint16_t port = 0; port < nb_ports; port++)
+    if (rte_eth_dev_socket_id(port) > 0 &&
+        rte_eth_dev_socket_id(port) !=
+            (int)rte_socket_id())
+      printf("WARNING, port %u is on remote NUMA node to "
+          "polling thread.\n\tPerformance will "
+          "not be optimal.\n", port);
 
-			if (unlikely(nb_rx == 0))
-				continue;
+  printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n", rte_lcore_id());
+  for (;;) {
+    for (uint16_t port = 0; port < nb_ports; port++) {
 
-			/* Send burst of TX packets, to second port of pair. */
-			const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0,
-					bufs, nb_rx);
+      struct rte_mbuf *bufs[BURST_SIZE];
+      const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
+          bufs, BURST_SIZE);
 
-			/* Free any unsent packets. */
-			if (unlikely(nb_tx < nb_rx)) {
-				uint16_t buf;
-				for (buf = nb_tx; buf < nb_rx; buf++)
-					rte_pktmbuf_free(bufs[buf]);
-			}
-		}
-	}
-}
+      if (unlikely(nb_rx == 0))
+        continue;
 
-/*
- * The main function, which does initialization and calls the per-lcore
- * functions.
- */
-int
-main(int argc, char *argv[])
-{
-	struct rte_mempool *mbuf_pool;
-	unsigned nb_ports;
-	uint16_t portid;
-
-	/* Initialize the Environment Abstraction Layer (EAL). */
-	int ret = rte_eal_init(argc, argv);
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
-
-	argc -= ret;
-	argv += ret;
-
-	/* Check that there is an even number of ports to send/receive on. */
-	nb_ports = rte_eth_dev_count();
-	if (nb_ports < 2 || (nb_ports & 1))
-		rte_exit(EXIT_FAILURE, "Error: number of ports must be even\n");
-
-	/* Creates a new mempool in memory to hold the mbufs. */
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-
-	if (mbuf_pool == NULL)
-		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
-
-	/* Initialize all ports. */
-	for (portid = 0; portid < nb_ports; portid++)
-		if (port_init(portid, mbuf_pool) != 0)
-			rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n",
-					portid);
-
-	if (rte_lcore_count() > 1)
-		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
-
-	/* Call lcore_main on the master core only. */
-	lcore_main();
-
-	return 0;
+      const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0, bufs, nb_rx);
+      if (unlikely(nb_tx < nb_rx)) {
+        uint16_t buf;
+        for (buf = nb_tx; buf < nb_rx; buf++)
+          rte_pktmbuf_free(bufs[buf]);
+      }
+    }
+  }
+  return 0;
 }
