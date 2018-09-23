3711f9ed392e1ca4e64ae07926d18ac085a8547a
Hiroki Shirokura <slankdev@nttv6.jp>
2018-09-23 11:38:22 +0000

    2018.09.23-11:38

--- netlinkd/main.c
+++ netlinkd/main.c
@@ -2,28 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include "netlink_socket.h"
+#include "libnetlink.h"
 #include "netlink_dump.h"
 
-int main(int argc, char** argv)
+static int
+dump_msg(const struct sockaddr_nl *who,
+         struct rtnl_ctrl_data *ctrl,
+		     struct nlmsghdr *n, void *arg)
 {
-  int sock = netlink_sock_open();
-  while (true) {
-    uint8_t buf[1000];
-    ssize_t ret = read(sock, buf, sizeof(buf));
-    if (ret < 0) {
-      return -1;
-    }
+  netlink_msg_dump(stdout, n);
+	return 0;
+}
 
-    for (struct nlmsghdr* hdr = (struct nlmsghdr*)buf;
-         NLMSG_OK(hdr, ret); hdr = NLMSG_NEXT(hdr, ret)) {
-      netlink_msg_dump(stdout, hdr);
-      printf("\n\n\n");
-    }
+/* static int */
+/* dump_msg2(const struct sockaddr_nl *who, */
+/* 		     struct nlmsghdr *n, void *arg) */
+/* { */
+/* 	return dump_msg(who, NULL, n, arg); */
+/* } */
 
-  } /* while (true) */
-  netlink_sock_close(sock);
+int
+main(int argc, char **argv)
+{
+	struct rtnl_handle rth;
+	uint32_t groups = ~0U;
+	if (rtnl_open(&rth, groups) < 0)
+		return 1;
+
+	/* if (rtnl_wilddump_request(&rth, AF_UNSPEC, RTM_GETLINK) < 0) { */
+	/* 	perror("Cannot send dump request"); */
+	/* 	return 1; */
+	/* } */
+
+	/* if (rtnl_dump_filter(&rth, dump_msg2, NULL) < 0) { */
+	/* 	fprintf(stderr, "Dump terminated\n"); */
+	/* 	return 1; */
+	/* } */
+
+	if (rtnl_listen(&rth, dump_msg, NULL) < 0)
+		return 2;
 }
 
